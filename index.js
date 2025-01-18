81ff19eedb53da3e705969995819beba01d1ea7d
abir718 <abir.hasan2003s@gmail.com>
2025-01-18 16:02:12 +0600

    connection made with mongodb

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//zFLDkdLnvR5wh7WF
+//cineflix
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umppp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("CineflixDB");
+    const addmovies = database.collection("addmovies");
+
+    app.post('/addmovies' , async(req , res)=>{
+        const newMovie = req.body;
+        console.log(newMovie);
+        const result = await coffee.insertOne(newMovie);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/' , (req , res)=>{
+    res.send('Server is running')
+})
+
+app.listen(port , () =>{
+    console.log(`Coffee server is running on port ${port}`)
+})
