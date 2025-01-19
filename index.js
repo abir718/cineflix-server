1ceb6b95fccd41ca50c8e9f7552cbd23c897ac88
abir718 <abir.hasan2003s@gmail.com>
2025-01-19 11:58:59 +0600

    delete operation for fav movie page added

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//zFLDkdLnvR5wh7WF
-//cineflix
+
 
 app.use(cors());
 app.use(express.json());
@@ -52,12 +51,33 @@ async function run() {
         res.send(result)
       })
 
+
+      
+
       app.post('/favmovies' , async(req , res)=>{
         const favMovie = req.body;
         console.log(favMovie);
+        const existingMovie = await favmovies.findOne({ email: favMovie.email, title: favMovie.title });
+        if (existingMovie) {
+            return res.status(400).json({ message: "Movie already in favorites" });
+        }
         const result = await favmovies.insertOne(favMovie);
         res.send(result);
     })
+
+    app.get('/favmovies' , async(req , res)=>{
+        const cursor = favmovies.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    app.delete('/favmovies/:id' , async (req , res)=>{
+        const id = req.params.id;
+        const query = {_id: id}
+        const result = await favmovies.deleteOne(query);
+        res.send(result)
+      })
 
 
     // Send a ping to confirm a successful connection
