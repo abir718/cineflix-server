00794bf550174f19252ac2a8707de56a5b1dac5e
abir718 <abir.hasan2003s@gmail.com>
2025-01-20 13:41:24 +0600

    vercel connection added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     const database = client.db("CineflixDB");
     const addmovies = database.collection("addmovies");
@@ -104,7 +104,7 @@ async function run() {
         res.send(result);
       });
       
-
+    
       app.put('/update/:id' , async(req , res)=>{
         const id = req.params.id;
         const filter = {_id: new ObjectId(id)}
@@ -121,7 +121,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
